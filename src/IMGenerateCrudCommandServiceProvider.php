f523a3d972f2d967c9544722f146d700b8fff45b
iMokhles <mokhleshussien@aol.com>
2020-04-13 15:46:49 +0200

    initial commit

---
+++ src/IMGenerateCrudCommandServiceProvider.php
@@ -0,0 +1,46 @@
+
+<?php
+
+namespace iMokhles\IMGenerateCrudCommand;
+
+use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Support\Composer;
+use Illuminate\Support\ServiceProvider;
+use iMokhles\IMGenerateCrudCommand\Command\IMGenerateCrudCommand;
+
+class IMGenerateCrudCommandServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->registerInstallCommand();
+    }
+
+    /**
+     * Register the make:multi-auth command.
+     */
+    private function registerInstallCommand()
+    {
+        $this->app->singleton('command.imokhles.make.im_crud', function ($app) {
+            return new IMGenerateCrudCommand();
+        });
+        $this->commands([
+            'IMGenerateCrudCommand' => 'command.imokhles.make.im_crud',
+        ]);
+    }
+}
